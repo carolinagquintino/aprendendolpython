ae1db8ca336e00ff6f9bb0792bd290e3e3817290
unknown <carolinagquintino@gmail.com>
2021-03-22 13:23:09 -0300

    add aula08

---
+++ Aulas/aula08_modulos.py
@@ -0,0 +1,23 @@
+# Módulos #
+
+# biblioteca math:
+    # ceil: arrendonda pra cima
+    # floor: arredonda pra baixo
+    # trunc: truncar (eliminar da virgula pra frente)
+    # pow: potência
+    # sqrt: raiz quadrada
+    # factorial: fatorial
+
+# import math
+# from math import sqrt
+
+# n = int(input('Digite um número: '))
+# raiz = sqrt(n)
+# print('A raiz de {} é {:.2f}'.format(n, raiz))
+
+# import random
+# num = random.randint(1, 10)
+# print(num)
+
+import emoji
+print(emoji.emojize('Olá :sunglasses:', use_aliases=True))
