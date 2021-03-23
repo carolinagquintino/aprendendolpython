b8b7ed93b8913143b53a2b716eb5e1b3a1fe9759
unknown <carolinagquintino@gmail.com>
2021-03-23 10:13:50 -0300

    finished challange 20 and 21, add directory Files

--- Desafios/desafio020.py
+++ Desafios/desafio020.py
@@ -1,3 +1,14 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresetação de trabalhos dos alunos. Faça um programa ...
 # que leia o nome dos quatro alunos e mostre a ordem sorteada
 
+import random
+
+a1 = input('Insira o nome de um aluno(a): ')
+a2 = input('Insira o nome de um aluno(a): ')
+a3 = input('Insira o nome de um aluno(a): ')
+a4 = input('Insira o nome de um aluno(a): ')
+
+lista = [a1, a2, a3, a4]
+
+print('A ordem de apredentação dos alunos será: {}'.format(random.sample(lista, k=4)))
+# random.shuffle(lista)
--- Desafios/desafio021.py
+++ Desafios/desafio021.py
@@ -1,1 +1,17 @@
-# Faaça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+
+# import pygame
+#
+# # Inicializando o mixer PyGame
+# pygame.mixer.init()
+#
+# # Iniciando o Pygame
+# pygame.init()
+#
+# pygame.mixer.music.load('Files/desafio08.mp3')
+# pygame.mixer.music.play(loops=0, start=0.0)
+# pygame.event.wait()
+
+
+import playsound
+playsound.playsound('Files/desafio08.mp3')
