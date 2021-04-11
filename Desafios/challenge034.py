d189aca5a8d637b0491511ee9e76ba360848cbd7
Carolina <carolinagquintino@gmail.com>
2021-04-11 12:08:36 -0300

    finished challenge 34

--- Desafios/challenge034.py
+++ Desafios/challenge034.py
@@ -1,3 +1,11 @@
 # Escreva um programa que pergunte o salário de um funcionário e calcula o valor do seu aumento. Para salários
 # superiores a R$1.250.00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%
 
+salario = float(input('Qual o seu salário? '))
+
+if salario <= 1250:
+    aumento = salario * 1.1
+    print('Parabéns, você recebeu um aumento de 10%! \n Seu novo salário será de R${:.2f}'.format(aumento))
+else:
+    aumento = salario * 1.15
+    print('Parabéns, você recebeu um aumento de 15%! Seu novo salário será de R${:.2f}'.format(aumento))
