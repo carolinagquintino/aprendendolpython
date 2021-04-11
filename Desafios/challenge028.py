6cc252988d377705828ce582e325e5fa5f828189
Carolina <carolinagquintino@gmail.com>
2021-04-11 11:11:51 -0300

    finished challange 28

--- Desafios/challenge028.py
+++ Desafios/challenge028.py
@@ -2,3 +2,12 @@
 # descobrir qual foi o número escolhido pelo computador. O Programa deverá escrever na tela se o usuário venceu ou
 # perdeu
 
+from random import randrange
+numPC = randrange(6)
+print('O computador escolheu um número entre 0 e 5.')
+numUser = int(input('Qual número você acha que o computador escolheu? '))
+
+if numUser == numPC:
+    print('Parabéns, você acertou! O número escolhido foi o {}'.format(numPC))
+else:
+    print('Que pena! O núemro escolhido foi o {}, você digitou {}'.format(numPC, numUser))
