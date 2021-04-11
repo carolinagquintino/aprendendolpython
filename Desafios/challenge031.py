e55463b46041d1b9e8737bf7f363710bf2689c2f
Carolina <carolinagquintino@gmail.com>
2021-04-11 11:25:45 -0300

    finished challenge 31

--- Desafios/challenge031.py
+++ Desafios/challenge031.py
@@ -1,3 +1,12 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50
 # por Km para viagens de até 200Km e R$0,45 para viagens mais longas
 
+dist = float(input('Qual a distância em Km da sua viagem? '))
+
+if dist <= 200:
+    valor = dist * 0.5
+    print('Sua passagem custará R${:.2f}'.format(valor))
+else:
+    valor = dist * 0.45
+    print('Sua passagem custará R${:.2f}'.format(valor))
+
