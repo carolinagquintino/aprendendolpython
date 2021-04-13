9ed7b3e93b0433c5cdec31da9294b79d90b74a41
unknown <carolinagquintino@gmail.com>
2021-04-13 11:53:41 -0300

    update challenge 32

--- Desafios/challenge032.py
+++ Desafios/challenge032.py
@@ -1,8 +1,19 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
 
-ano = int(input('Digite um ano: '))
+from datetime import date
 
-if str(ano)[1:].strip() == '00':
+ano = input('Digite um ano ou "atual" para o ano atual: ')
+
+if ano == 'atual':
+    ano = date.today().year  # verifica o ano atual da máquina
+
+if int(ano) % 4 == 0 and int(ano) % 100 != 0 or int(ano) % 400 == 0:
+    print('O ano {} é bissexto'.format(ano))
+else:
+    print('O ano {} não é bissexto'.format(ano))
+
+'''
+if str(ano)[2:].strip() == '00':
     if ano % 400 == 0:
         print('O ano {} é bissexto'.format(ano))
     else:
@@ -11,3 +22,4 @@ elif ano % 4 == 0:
     print('O ano {} é bissexto'.format(ano))
 else:
     print('O ano {} não é bissexto'.format(ano))
+'''
