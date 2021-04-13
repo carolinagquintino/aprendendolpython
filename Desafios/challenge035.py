4014d6dcf69379d8c9664e9c67ccab96e7bd8657
unknown <carolinagquintino@gmail.com>
2021-04-13 13:55:11 -0300

    challenge 35

--- Desafios/challenge035.py
+++ Desafios/challenge035.py
@@ -5,18 +5,7 @@ l1 = float(input('Digite o comprimento de uma reta: '))
 l2 = float(input('Digite o comprimento da segunda reta: '))
 l3 = float(input('Digite o comprimento da última reta: '))
 
-if l1 > l2 and l1 > l3:
-    if l2 + l3 <= l1:
-        print('Vixe, essas retas não formam um triângulo.')
-    else:
-        print('Eba! Essas retas formam um triângulo')
-elif l2 > l1 and l2 > l3:
-    if l1 + l3 <= l2:
-        print('Vixe, essas retas não formam um triângulo.')
-    else:
-        print('Eba! Essas retas formam um triângulo')
-elif l3 > l1 and l3 > l2:
-    if l1 + l2 <= l3:
-        print('Vixe, essas retas não formam um triângulo.')
-    else:
-        print('Eba! Essas retas formam um triângulo')
+if l1 < l2 + l3 and l2 < l3 + l1 and l3 < l1 + l2:
+    print('Eba! Essas retas formam um triângulo')
+else:
+    print('Vixe, essas retas não formam um triângulo.')
