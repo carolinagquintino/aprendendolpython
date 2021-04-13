14b198048c8c0cc9dbdb81dcb19e088122ee1a8d
unknown <carolinagquintino@gmail.com>
2021-04-13 13:42:25 -0300

    update challenge 33

--- Desafios/challenge033.py
+++ Desafios/challenge033.py
@@ -1,12 +1,23 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor
 
-n1 = float(input('Digite um número: '))
-n2 = float(input('Digite outro número: '))
-n3 = float(input('Último número a ser digitado: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Último número a ser digitado: '))
 
+# Verifica maior
 if n1 > n2 and n1 > n3:
-    print('O primeiro número, {}, é o maior entre {}, {} e {}'.format(n1, n1, n2, n3))
+    maior = n1
 elif n2 > n1 and n2 > n3:
-    print('O segundo número, {}, é o maior entre {}, {} e {}'.format(n2, n1, n2, n3))
+    maior = n2
 elif n3 > n1 and n3 > n2:
-    print('O terceiro número, {}, é o maior entre {}, {} e {}'.format(n3, n1, n2, n3))
+    maior = n3
+
+# Verifica menor
+if n1 < n2 and n1 < n3:
+    menor = n1
+elif n2 < n1 and n2 < n3:
+    menor = n2
+elif n3 < n1 and n3 < n2:
+    menor = n3
+
+print('O maior número é o {} e o menor é o {}'.format(maior, menor))
