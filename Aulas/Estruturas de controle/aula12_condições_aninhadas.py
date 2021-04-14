d32767401b32b13c4fd0c0309fc25c8eb0e42db5
unknown <carolinagquintino@gmail.com>
2021-04-14 15:57:00 -0300

    add class 12

---
+++ Aulas/Estruturas de controle/aula12_condições_aninhadas.py
@@ -0,0 +1,19 @@
+'''
+Condições aninhadas
+
+if -condição-:
+elif -condição-:
+else:
+
+'''
+
+nome = input('Qual é seu nome?')
+
+if nome == 'Carolina':
+    print('Que nome bonito!')
+elif nome == 'Ana':
+    print('Seu nome é bem popular.')
+elif nome == 'Pedro':
+    print('Belo nome masculino.')
+else:
+    print('Seu nome é bem normal.')
