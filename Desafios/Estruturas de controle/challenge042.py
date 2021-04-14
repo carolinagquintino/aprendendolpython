0b9f8d878033a86b4dc062e9826d79f642ffd266
unknown <carolinagquintino@gmail.com>
2021-04-14 16:40:22 -0300

    add challenge 36 to 45

---
+++ Desafios/Estruturas de controle/challenge042.py
@@ -0,0 +1,6 @@
+'''
+Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- Equilátero: todos os lados iguais
+- Esósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+'''
