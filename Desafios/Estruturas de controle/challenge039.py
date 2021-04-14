fda9a415220d8d049f6a53ebde35876f362a34ec
unknown <carolinagquintino@gmail.com>
2021-04-14 17:51:27 -0300

    update challenge 39

--- Desafios/Estruturas de controle/challenge039.py
+++ Desafios/Estruturas de controle/challenge039.py
@@ -5,3 +5,8 @@ Faça um programa que leia o ano de nascimento de um jovem e imforme, de acordo 
 - Se já passou do tempo do alistamento
 Seu programa também deverá mostrar o tempo que falta ou que passou do prazo
 '''
+
+from datetime import date
+
+idade = int(input('Qual a sua idade?'))
+ano = date.today().year
