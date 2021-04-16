8c4fae9eee2cdcd4d27aa6a2760b69a4f80d7b39
unknown <carolinagquintino@gmail.com>
2021-04-16 11:25:13 -0300

    finished challenge 40

--- Desafios/Estruturas de controle/challenge040.py
+++ Desafios/Estruturas de controle/challenge040.py
@@ -4,3 +4,17 @@ Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando
 - Média entre 5.0 e 6.9: RECUPERAÇÃO
 - Média 7.0 ou superior: APROVADO
 '''
+
+nota1 = float(input('Qual foi a nota obtida na primeira avaliação? '))
+nota2 = float(input('Qual foi a nota obtida na segunda avaliação? '))
+
+media = (nota1 + nota2) / 2
+
+print('Sua média foi de {:.1f}'.format(media))
+
+if media < 5:
+    print('Sinto muito, mas essa pontuação não é suficiente para aprovação!')
+elif media < 6.9:
+    print('Sinto muito, mas essa pontuação indica recuperação!')
+else:
+    print('Parabéns! Você foi aprovado.')
